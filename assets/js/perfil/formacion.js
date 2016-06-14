4ae8f68019b11a82eda80ff5ba01db3df46f2764
Ricardo Sánchez Saldivar <ricsasa201@gmail.com>
2016-06-14 09:53:31 -0500

    Frontend

--- assets/js/perfil/formacion.js
+++ assets/js/perfil/formacion.js
@@ -3,9 +3,9 @@ $(function () {
     $('#modalFormacionSalud').on('show.bs.modal', function (e) {
         var idFormacion = $(e.relatedTarget).data('idformacion');
         var opcion = $(e.relatedTarget).data('opcion');
-        console.log(idFormacion);
-        console.log(opcion);
-        
+        if(idFormacion !== 0){
+            $.ajax();
+        }
         //store data as JSON, beware of enclose it with double quotes     
     });    
 });
--- assets/js/perfil/informacionGeneral.js
+++ assets/js/perfil/informacionGeneral.js
@@ -4,4 +4,16 @@ $(function(){
         $('.nameFields').prop('readonly', !isReadOnly);
         $('#perfil_apellido_paterno').focus();
     });
+    
+    $("ul.nav-pills > li > a").on("shown.bs.tab", function (e) {
+       var scrollposition = $(document).scrollTop();
+       var id = jQuery(e.target).attr("href").substr(1);
+       window.location.hash = id;
+       $(document).scrollTop(scrollposition);
+    });
+
+    var hash = window.location.hash;
+    $('.nav.nav-pills a[href="' + hash + '"]').tab('show', function(){
+        $(document).scrollTop();
+    });
 });
